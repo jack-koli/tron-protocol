28277de33dad1d0e88265d326ba1768316ebb925
Poo Jose <pokiteg@gmail.com>
2020-11-07 07:30:57 +0800

    test

--- gen-proto.sh
+++ gen-proto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # gen pb.go
-protoc --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. core/contract/*.proto
+protoc  --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. core/contract/*.proto
 protoc --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. core/*.proto
 protoc --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. api/*.proto
